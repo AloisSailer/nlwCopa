87f235410f33bf17595f24c45332d66bc0b7b477
Alois Sailer <alois_sailer@yahoo.com.br>
2022-10-28 22:11:48 -0300

    terminado

--- figurinhas/script.js
+++ figurinhas/script.js
@@ -1,3 +1,7 @@
+let isIgnite = false
 function changeCard(event) {
-  console.log(event.currentTarget)
+  const card = event.currentTarget
+  const bg = isIgnite ? 'explorer' : 'ignite'
+  isIgnite = !isIgnite
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
 }
